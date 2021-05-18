357b47ec951fa6fef0d013ccc8c3a328b2e89540
Shubham Hibare <github@hibare.in>
2021-05-18 19:29:57 +0530

    Update serializer

--- backend/jobs/serializers.py
+++ backend/jobs/serializers.py
@@ -6,9 +6,26 @@ from rest_framework import serializers
 
 
 class JobsSerializer(serializers.HyperlinkedModelSerializer):
+    uuid = serializers.CharField(max_length=40, read_only=True)
+    url = serializers.URLField(
+        help_text="URL to run health check against", required=True)
+    title = serializers.CharField(
+        max_length=40, help_text="Title for easy identification of this job", required=True)
+    state = serializers.BooleanField(
+        default=True, initial=True, help_text="State of the job")
+    notify_url = serializers.URLField(
+        help_text="Notification URL used when healthcheck fails")
+    verify_ssl = serializers.BooleanField(
+        default=True, initial=True, help_text="Verify SSL certificate")
+    interval = serializers.IntegerField(
+        initial=15, help_text="Job interval in minutes")
+    success_status = serializers.IntegerField(
+        default=200, initial=200, help_text="HTTP status to check health check response status for success")
+    check_redirect = serializers.BooleanField(
+        default=True, initial=True, help_text="Check for redirects")
 
     class Meta:
         model = Jobs
-        fields = ['url', 'title', 'state',
+        fields = ['uuid', 'url', 'title', 'state',
                   'notify_url', 'verify_ssl', 'interval', 'success_status', 'check_redirect']
-        read_only_fields = ['uuid']
+        read_only_fields = ['']
