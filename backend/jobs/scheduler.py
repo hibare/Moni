f240c872cefe4b5a9902995ca1fd3eb9bc6bfb0a
Shubham Hibare <github@hibare.in>
2021-05-23 20:55:19 +0530

    Add job to delete execution records

--- backend/jobs/scheduler.py
+++ backend/jobs/scheduler.py
@@ -3,7 +3,9 @@
 import logging
 from django.conf import settings
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 from django_apscheduler.jobstores import register_events
+from django_apscheduler.models import DjangoJobExecution
 
 
 logger = logging.getLogger(__name__)
@@ -17,6 +19,11 @@ def test_job():
     logger.info("Test job executed")
 
 
+def delete_old_job_executions(max_age=604_800):
+    """This job deletes all apscheduler job executions older than `max_age` from the database."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
 def start():
     """Start Scheduler"""
 
@@ -24,6 +31,19 @@ def start():
         # Hook into the apscheduler logger
         logging.basicConfig()
         logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),  # Midnight on Monday, before start of the next work week.
+            id="delete_old_job_executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info(
+            "Added weekly job: 'delete_old_job_executions'."
+        )
 
     # scheduler.add_job(test_job,
     #                   "interval", id="test_job", minutes=5, replace_existing=True)
