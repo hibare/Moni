0d163191b948c8a44f61de84900059be56393db9
Shubham Hibare <github@hibare.in>
2021-07-18 17:22:07 +0530

    Change job history endpoint + add job resume & pause endpoint

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,12 +1,15 @@
 """Jobs Views"""
 
-from rest_framework import viewsets, mixins, generics
+import logging
+from rest_framework import viewsets, mixins, generics, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from jobs.models import Jobs, JobsHistory
 from jobs.serializers import JobsSerializer, JobsHistorySerializer
 
+logger = logging.getLogger(__name__)
 
 class JobsViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """Jobs ViewSet"""
@@ -16,30 +19,67 @@ class JobsViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.UpdateM
     serializer_class = JobsSerializer
     queryset = Jobs.objects.all()
 
+    @action(methods=['get'], detail=True, permission_classes=[IsAuthenticated])
+    def history(self, request, **kwargs):
+        """Return Job execution history"""
 
-class JobsHistoryViewSet(generics.ListAPIView, viewsets.GenericViewSet, generics.RetrieveAPIView):
+        uuid = self.kwargs['uuid']
+
+        queryset = JobsHistory.objects.filter(uuid=uuid)
+
+        if queryset.exists():
+            serializer = JobsHistorySerializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            raise NotFound()
+
+    @action(methods=['get'], detail=True, permission_classes=[IsAuthenticated])
+    def pause(self, request, **kwargs):
+        """Pause job"""
+
+        try:
+            uuid = self.kwargs['uuid']
+
+            job = self.queryset.get(uuid=uuid)
+
+            if job.state:
+                job.state = False
+                job.save()
+
+                return Response({"detail": "Job paused"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"detail": "Job already in pause state"}, status=status.HTTP_409_CONFLICT)
+        except Jobs.DoesNotExist:
+            raise NotFound
+
+    @action(methods=['get'], detail=True, permission_classes=[IsAuthenticated])
+    def resume(self, request, **kwargs):
+        """Resume job"""
+
+        try:
+            uuid = self.kwargs['uuid']
+
+            job = self.queryset.get(uuid=uuid)
+
+            if not job.state:
+                job.state = True
+                job.save()
+
+                return Response({"detail": "Job resumed"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"detail": "Job already in active state"}, status=status.HTTP_409_CONFLICT)
+        except Jobs.DoesNotExist:
+            raise NotFound
+
+
+class JobsHistoryViewSet(generics.ListAPIView, viewsets.GenericViewSet):
     """
     Jobs History ViewSet
 
     /api/v1/jobs/history/
-    /api/v1/jobs/history/uuid=97e488cf-d847-4668-a6d9-1ee7dc0f00b2/
     """
 
     lookup_field = "uuid"
     permission_classes = (IsAuthenticated,)
     serializer_class = JobsHistorySerializer
     queryset = JobsHistory.objects.all()
-
-    def retrieve(self, request, *args, **kwargs):
-        uuid = self.kwargs.get('uuid')
-
-        if uuid is None:
-            return self.queryset()
-        else:
-            queryset = self.queryset.filter(uuid=uuid)
-
-            if queryset.exists():
-                serializer = self.get_serializer(queryset, many=True)
-                return Response(serializer.data)
-            else:
-                raise NotFound()
