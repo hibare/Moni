e8474193deca1288986591d8c3039070928633f0
Shubham Hibare <github@hibare.in>
2021-07-18 16:23:29 +0530

    Fix history endpoint

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,6 +1,8 @@
 """Jobs Views"""
 
 from rest_framework import viewsets, mixins, generics
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from jobs.models import Jobs, JobsHistory
 from jobs.serializers import JobsSerializer, JobsHistorySerializer
@@ -15,15 +17,29 @@ class JobsViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.UpdateM
     queryset = Jobs.objects.all()
 
 
-class JobsHistoryViewSet(generics.ListAPIView, viewsets.GenericViewSet):
+class JobsHistoryViewSet(generics.ListAPIView, viewsets.GenericViewSet, generics.RetrieveAPIView):
     """
     Jobs History ViewSet
 
     /api/v1/jobs/history/
-    /api/v1/jobs/history?uuid=97e488cf-d847-4668-a6d9-1ee7dc0f00b2
+    /api/v1/jobs/history/uuid=97e488cf-d847-4668-a6d9-1ee7dc0f00b2/
     """
 
     lookup_field = "uuid"
     permission_classes = (IsAuthenticated,)
     serializer_class = JobsHistorySerializer
     queryset = JobsHistory.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        uuid = self.kwargs.get('uuid')
+
+        if uuid is None:
+            return self.queryset()
+        else:
+            queryset = self.queryset.filter(uuid=uuid)
+
+            if queryset.exists():
+                serializer = self.get_serializer(queryset, many=True)
+                return Response(serializer.data)
+            else:
+                raise NotFound()
