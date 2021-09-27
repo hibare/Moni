6fc49026160e1c177e4f48d1b07e17f12ab5386b
Shubham Hibare <github@hibare.in>
2021-09-27 18:00:46 +0530

    Create initial vue project + add vuetify

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <b>Strong</b>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
