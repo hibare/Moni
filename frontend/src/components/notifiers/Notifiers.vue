672fe774880a11057254de41fa935a60a691bf65
Shubham Hibare <github@hibare.in>
2021-12-11 20:03:52 +0530

    Remove exposed secrets

--- frontend/src/components/notifiers/Notifiers.vue
+++ frontend/src/components/notifiers/Notifiers.vue
@@ -6,11 +6,4 @@
 export default {
   name: "Notifiers",
 };
-/*
-
-"""
-curl --request GET   --url 'http://127.0.0.1:5000/api/v1/notifiers/?type=telegram'   --header 'X-Access-Token: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM1MDczNzE5LCJqdGkiOiI4YTY5NDE5OWU2NDI0MjVkYjNhYzFlMjlkOWJmZjMwZSIsInVzZXJfaWQiOjF9.9Pz28lvFbUdeQN_oG-1HjQoRmz7OCCb6WUa1_31M6TU'
-[{"uuid":"3507137f-9628-4439-bcf9-c3465f0b0528","created":"2021-10-24T09:23:50.852138Z","updated":"2021-10-24T09:23:50.852157Z","url":"https://api.telegram.org/bot1906444821:AAG2fPhNLOYCsqTt2e2tS731l_166HfCOQk/sendMessage?chat_id=-1001583022725","type":"telegram","description":"teltgra,"}]
-"""
-*/
 </script>
