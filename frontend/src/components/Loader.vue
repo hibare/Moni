0ae8477fb6a99a3cd109f517eb43d2d101cafd91
Shubham Hibare <shubham@hibare.in>
2022-12-27 21:54:39 +0530

    Validate token

--- frontend/src/components/Loader.vue
+++ frontend/src/components/Loader.vue
@@ -1,11 +1,9 @@
 <template>
   <v-container fill-height fluid>
-    <v-row align="center" justify="center">
-      <v-progress-circular
-        :size="50"
-        color="primary"
-        indeterminate
-      ></v-progress-circular>
+    <v-row justify="center" class="text-center">
+      <v-col>
+        <v-progress-circular :size="50" color="primary" indeterminate></v-progress-circular>
+      </v-col>
     </v-row>
   </v-container>
 </template>
