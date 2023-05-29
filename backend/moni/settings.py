adb6e9577981cd5dde005308e310dd3d3df8cb6f
Shubham Hibare <shubham@hibare.in>
2023-05-29 20:06:46 +0530

    fix type error + minor refactor

--- backend/moni/settings.py
+++ backend/moni/settings.py
@@ -10,16 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from moni import TITLE, VERSION
 from pathlib import Path
 from dj_database_url import parse as db_url
 from decouple import config, Csv
 from datetime import timedelta
+from moni import TITLE, VERSION
 from moni.utils.funcs import get_version, get_title
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -145,7 +144,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Logging
 
-LOG_LEVEL = config("LOG_LEVEL", default="INFO", cast=str).upper()
+LOG_LEVEL = config("LOG_LEVEL", default="INFO",
+                   cast=str).upper()  # type: ignore
 
 LOGGING = {
     'version': 1,
